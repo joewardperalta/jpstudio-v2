7749cc5b62c5f37642fb77b6453e29a364bc0218
Joeward Peralta <joeward_peralta@outlook.com>
2026-03-20 13:46:09 -0400

    Re-organize code

--- assets/javascript/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import loadNavbar from "/assets/javascript/components/navbar.js";
-
-// Load navbar for each page that includes it
-if (document.getElementById("navbar")) {
-  loadNavbar();
-}
---
+++ js/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+import loadNavbar from "../js/components/navbar.js";
+
+// Load the navbar component if the current page includes it
+if (document.getElementById("navbar")) {
+  loadNavbar();
+}
